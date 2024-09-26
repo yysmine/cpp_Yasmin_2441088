f8c9f07ecfa52aa45361e9db2f8a4c4cfb5059a7
Yasmin Dzakkiya <yasminnndzkkya06@gmail.com>
2024-09-26 22:46:55 +0700

    Add files via upload

---
+++ Yasmin_Dzakkiya_2441088_1.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	
+	printf("Nama          : YASMIN DZAKKIYA.\n");
+	printf("NIM           : 2441088.\n");
+	printf("Jurusan       : TEKNIK INFORMATIKA.\n");
+	
+	return 0;
+}
---
+++ Yasmin_Dzakkiya_2441088_2.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int PN, LB, LS;
+	
+	PN=10;
+	
+	LB=7;
+	
+	printf("Diketahui panjang persegi panjang 10CM.\n");
+	
+	printf("Dan lebar  persegi panjang 7CM.\n");
+	
+	printf("Maka luas persegi panjang adalah...?\n");
+	
+	LS=PN*LB;
+	
+	printf("%d",LS);
+	
+	return 0;
+}
